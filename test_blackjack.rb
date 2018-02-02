04566cf1cdd12a089f1794465ed7b1f9fce1f46c
ashlynnpai <ashlynnpai@gmail.com>
2018-02-02 16:43:59 -0500

    add more test cases for total method

--- test_blackjack.rb
+++ test_blackjack.rb
@@ -2,35 +2,43 @@ require 'minitest/autorun'
 require './blackjack'
 
 class TestBlackjack < Minitest::Test
-  def test_two_aces_with_different_values
-    deck = Deck.new
-    def deck.create_deck
-      card_values = [2, 'A', 'A']
-      suits = ['H']
-      @cards = []
-      card_values.each do |card_value|
-        suits.each do |suit|
-          @cards << Card.new(suit, card_value)
-        end
+
+  def create_deck_stub(card_values)
+    suits = ['H']
+    @cards = []
+    card_values.each do |card_value|
+      suits.each do |suit|
+        @cards << Card.new(suit, card_value)
       end
-      @cards
     end
-    hand = deck.create_deck
-    puts hand
+    @cards
+  end
+
+  def test_one_ace_with_value_11
     player = Player.new
+    hand = create_deck_stub([2, 'A', 8])
+    player.cards = hand
+    assert_equal 21, player.total(player.cards)
+  end
+
+  def test_one_ace_value_changes_to_1
+    player = Player.new
+    hand = create_deck_stub([2, 'A', 9])
+    player.cards = hand
+    assert_equal 12, player.total(player.cards)
+  end
+
+  def test_two_aces_with_different_values
+    player = Player.new
+    hand = create_deck_stub([2, 'A', 'A'])
     player.cards = hand
     assert_equal 14, player.total(player.cards)
   end
+
+  def test_three_two_aces
+    player = Player.new
+    hand = create_deck_stub(['A', 'A', '3', 'A', '5'])
+    player.cards = hand
+    assert_equal 21, player.total(player.cards)
+  end
 end
-
-
-
-# 2 A A should be 14
-#
-# 2 3 A should be 16
-#
-# 2 A 8 should be 21
-#
-# 2 A 9 should be 12
-#
-# A 3 2 8 should equal 14
